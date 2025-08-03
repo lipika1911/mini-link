97fb56d7d89a80091a7d79e424b981cb79ff4ced
lipika1911 <lipikaarya1723@gmail.com>
2025-08-03 21:03:49 +0530

    feat: integrate UAParser for click tracking

--- src/pages/RedirectLink.jsx
+++ src/pages/RedirectLink.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { storeClicks } from '@/db/apiClicks';
+import { getLongUrl} from '@/db/apiUrls';
+import UseFetch from '@/hooks/UseFetch';
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { BarLoader } from 'react-spinners';
 
 const RedirectLink = () => {
-  return (
-    <div>RedirectLink</div>
-  )
+
+  const {id} = useParams();
+  
+  const {loading, data, fn} = UseFetch(getLongUrl, id);
+
+  const {loading: loadingStats, fn: fnStats} = UseFetch(storeClicks, {
+    id: data?.id,
+    originalUrl: data?.original_url,
+  })
+
+  useEffect(()=>{
+    fn()
+  },[])
+
+  useEffect(()=>{
+    if(!loading && data){
+      fnStats();
+    }
+  },[loading])
+
+  if (loading || loadingStats){
+    return(
+      <>
+        <BarLoader className='mx-16' width={"100%"} color='#36d7b7' />
+        <br />
+        <div className='mx-16'>
+          Redirecting...
+        </div>
+      </>
+    )
+  }
+
+  return null
 }
 
 export default RedirectLink
